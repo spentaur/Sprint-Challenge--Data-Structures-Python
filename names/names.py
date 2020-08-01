694680307941e9322f3b223373c0ae7bf6c81e81
Spencer Adams <spenceradams@Spencers-MacBook-Pro.local>
2020-07-31 23:02:41 -0500

    uncommented the original in names

--- names/names.py
+++ names/names.py
@@ -1,29 +1,29 @@
 from BinarySearch import BSTNode
 import time
 
-# start_time = time.time()
+start_time = time.time()
 
-# f = open('names_1.txt', 'r')
-# names_1 = f.read().split("\n")  # List containing 10000 names
-# f.close()
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
 
-# f = open('names_2.txt', 'r')
-# names_2 = f.read().split("\n")  # List containing 10000 names
-# f.close()
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
 
-# duplicates = []  # Return the list of duplicates in this data structure
+duplicates = []  # Return the list of duplicates in this data structure
 
-# # Replace the nested for loops below with your improvements
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
+# Replace the nested for loops below with your improvements
+for name_1 in names_1:
+    for name_2 in names_2:
+        if name_1 == name_2:
+            duplicates.append(name_1)
 
-# end_time = time.time()
-# print("ORIGINAL:")
-# print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-# print(f"runtime: {end_time - start_time} seconds")
-# print(f"\n{'-'*100}\n")
+end_time = time.time()
+print("ORIGINAL:")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
+print(f"\n{'-'*100}\n")
 
 
 start_time = time.time()
